2c974ee1e19baae54b494bb1079f5520753116ba
sonjikyeong <thswlrud0825@daum.net>
2018-03-27 11:22:44 +0900

    로그인 암호화

--- src/main/java/com/dankook/hub/service/UserServiceImpl.java
+++ src/main/java/com/dankook/hub/service/UserServiceImpl.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.dankook.hub.dao.UserDAO;
+import com.dankook.hub.utils.SHA256Util;
 import com.dankook.hub.vo.LoginVO;
 import com.dankook.hub.vo.UserVO;
 
@@ -27,18 +28,24 @@ public class UserServiceImpl implements UserService {
         HashMap<String, Object> map = new HashMap<>();
         
         UserVO uvo = dao.selectUser(lvo.getUsr_id());
-        if(uvo != null) { // id 존재
-            if(uvo.getUsr_password().equals(lvo.getUsr_password())){ // 로그인 성공
+        /* uvo password 를  hash에서 정상으로 변환*/
+        String memberSalt = uvo.getUsr_salt();
+        String inputPassword = lvo.getUsr_password();
+        String newPassword = SHA256Util.getEncrypt(inputPassword, memberSalt);
+        lvo.setUsr_password(newPassword);
+        
+        if(uvo != null) { // id 議댁옱
+            if(uvo.getUsr_password().equals(lvo.getUsr_password())){ // 濡쒓렇�씤 �꽦怨�
                 uvo.setUsr_password("");
                 map.put("result", true);
                 map.put("user", uvo);
                 return map;
-            } else { // password 틀림
+            } else { // password ��由�
                 map.put("result", false);
                 map.put("fail", "password");
                 return map;
             }
-        } else { // id 틀림
+        } else { // id ��由�
             map.put("result", false);
             map.put("fail", "id");
             return map;
@@ -48,6 +55,12 @@ public class UserServiceImpl implements UserService {
     @Override
     public void insertUser(UserVO uvo) {
         System.out.println("UserService.addUser() called...");
+        
+        /* 받아온 password값을 SHA256 으로 암호화*/
+        String salt = SHA256Util.generateSalt();
+        String newPassword = SHA256Util.getEncrypt(uvo.getUsr_password(), salt);
+        uvo.setUsr_password(newPassword);
+        uvo.setUsr_salt(salt);
         
         dao.insertUser(uvo);
     }
--- src/main/java/com/dankook/hub/vo/LoginVO.java
+++ src/main/java/com/dankook/hub/vo/LoginVO.java
@@ -3,6 +3,15 @@ package com.dankook.hub.vo;
 public class LoginVO {
     private String usr_id;
     private String usr_password;
+    private String usr_salt;
+    
+    
+	public String getUsr_salt() {
+		return usr_salt;
+	}
+	public void setUsr_salt(String usr_salt) {
+		this.usr_salt = usr_salt;
+	}
 	public String getUsr_id() {
 		return usr_id;
 	}
